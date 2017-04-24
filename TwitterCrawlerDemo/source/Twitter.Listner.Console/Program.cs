eaf218924d30a6f2eb813dd7269da4e41826639c
Dmitry Petukhov <dmitry.petukhov@live.ru>
2017-04-24 09:05:14 +0000

    Remove redundant namespaces

--- TwitterCrawlerDemo/source/Twitter.Listner.Console/Program.cs
+++ TwitterCrawlerDemo/source/Twitter.Listner.Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Twitter.Streaming;
+﻿using Twitter.Streaming;
 using Twitter.Streaming.Configurations;
 
 namespace Twitter.Listner.Console
