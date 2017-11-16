7fde452722e9768c147b0f36d6702ce752d9501f
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2017-11-16 18:39:45 +0300

    Remove redundant commands

--- BitcoinTimeSeriesAnalysis/source/install_scripts.sh
+++ BitcoinTimeSeriesAnalysis/source/install_scripts.sh
@@ -8,21 +8,18 @@ sudo ./install.sh
 
 
 ### Install MS R Client and/or MS ML Server  ----
-sudo su
-apt-get install apt-transport-https
-
 wget http://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb 
 dpkg -i packages-microsoft-prod.deb
 ls -la /etc/apt/sources.list.d
 
-apt-get update
+sudo apt-get update
 
 # for R Client
-apt-get install microsoft-r-client-packages-3.4.1
+sudo apt-get install microsoft-r-client-packages-3.4.1
 ls /opt/microsoft/rclient/3.4.1/
 
 # for ML Server
-apt-get install microsoft-mlserver-all-9.2.1
+sudo apt-get install microsoft-mlserver-all-9.2.1
 /opt/microsoft/mlserver/9.2.1/bin/R/activate.sh
 
 
@@ -35,10 +32,3 @@ sudo gdebi rstudio-server-1.1.383-amd64.deb
 
 # open SSH tunnel
 ssh -N -L 8787:localhost:8787 dp@rclient.westus.cloudapp.azure.com
-
-# Set R Client version as default (do not work)
-#sudo nano /etc/rstudio/rserver.conf
-#rsession-which-r=/opt/microsoft/rclient/3.4.1/bin/R/R
-
-#export RSTUDIO_WHICH_R=/opt/microsoft/rclient/3.4.1/bin/R/R/
-#sudo rstudio-server restart
