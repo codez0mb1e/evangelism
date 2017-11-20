d0cd1ffeed91e32ffb349f861741224c66b320c6
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2017-11-20 16:53:02 +0300

    Minor fixes

--- BitcoinTimeSeriesAnalysis/source/install_scripts.sh
+++ BitcoinTimeSeriesAnalysis/source/install_scripts.sh
@@ -9,23 +9,23 @@ sudo ./install.sh
 
 ### Install MS R Client and/or MS ML Server  ----
 wget http://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb 
-dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 ls -la /etc/apt/sources.list.d
 
 sudo apt-get update
 
-# for R Client
+# Install MS R Client
 sudo apt-get install microsoft-r-client-packages-3.4.1
 ls /opt/microsoft/rclient/3.4.1/
 
-# for ML Server
+# Install MS ML Server
 sudo apt-get install microsoft-mlserver-all-9.2.1
 /opt/microsoft/mlserver/9.2.1/bin/R/activate.sh
 
 
 
 ### Advanced tools ----
-# install RStudio Server
+# Install RStudio Server
 sudo apt-get install gdebi-core
 wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
 sudo gdebi rstudio-server-1.1.383-amd64.deb
@@ -36,19 +36,22 @@ export RSTUDIO_WHICH_R=/usr/bin/Revo64
 sudo nano /etc/rstudio/rserver.conf # add key to config: rsession-which-r=/usr/bin/Revo64
 
 
-# open SSH tunnel
+# open SSH tunnel (optional)
 ssh -N -L 8787:localhost:8787 <user>@<host>.cloudapp.azure.com #! specify user and host
 
 
 
-### Install dependencies 
-# for R packages
+### Install dependencies:
+# R packages builders
 sudo apt-get install build-essential
 sudo apt-get install libcurl4-openssl-dev libssl-dev
 sudo apt-get install gfortran
-# (optional) .NET Core installation: https://www.microsoft.com/net/learn/get-started/linuxubuntu
-# for source control
+
+# source control
 sudo apt-get install git
-# for monitoring
+
+# monitoring
 sudo apt-get install htop
+
+# (optional) .NET Core installation: https://www.microsoft.com/net/learn/get-started/linuxubuntu
 
