558df8253d2eab9d0eb0cf4dfd8b8c535d90706a
Dmitry Petukhov <dmitry.petukhov@outlook.com>
2017-11-21 18:48:42 +0300

    Git + VSTS integration. Minor fixes

--- BitcoinTimeSeriesAnalysis/source/install_scripts.sh
+++ BitcoinTimeSeriesAnalysis/source/install_scripts.sh
@@ -25,7 +25,7 @@ sudo apt-get install microsoft-mlserver-all-9.2.1
 
 
 ### Advanced tools ----
-# Install RStudio Server
+## Install RStudio Server
 sudo apt-get install gdebi-core
 wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
 sudo gdebi rstudio-server-1.1.383-amd64.deb
@@ -35,23 +35,34 @@ export RSTUDIO_WHICH_R=/usr/bin/Revo64
 # or 
 sudo nano /etc/rstudio/rserver.conf # add key to config: rsession-which-r=/usr/bin/Revo64
 
-
-# open SSH tunnel (optional)
+# open SSH tunnel (usually already open)
 ssh -N -L 8787:localhost:8787 <user>@<host>.cloudapp.azure.com #! specify user and host
 
 
-
-### Install dependencies:
-# R packages builders
+## Install R packages builders dependencies
 sudo apt-get install build-essential
 sudo apt-get install libcurl4-openssl-dev libssl-dev
 sudo apt-get install gfortran
 
-# source control
-sudo apt-get install git
 
-# monitoring
+## Monitoring/admin utils
 sudo apt-get install htop
 
-# (optional) .NET Core installation: https://www.microsoft.com/net/learn/get-started/linuxubuntu
+
+## .NET Core (optional)
+# see: https://www.microsoft.com/net/learn/get-started/linuxubuntu
+
+
+## Git + VSTS integration 
+sudo apt-get install git
+
+ssh-keygen -C "<azure_user_name>" # create ssh-key
+cat /home/<user>/.ssh/id_rsa.pub # view public created key
+# register public key to VSTS. See: https://www.visualstudio.com/en-us/docs/git/use-ssh-keys-to-authenticate
+ssh -T <VSTS_account_name>@<VSTS_account_name>.visualstudio.com # test connection
+
+mkdir /home/<user>/apps
+cd  /home/<user>/apps
+git clone ssh://<VSTS_account_name>@vs-ssh.visualstudio.com:22/DefaultCollection/_ssh/<project_name> # clone repository
+
 
