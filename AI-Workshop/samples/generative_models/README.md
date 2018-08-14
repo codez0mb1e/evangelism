aed0499f01526373f5b269250372a0328dcd0e69
Dmitry Petukhov <d.petukhov@outlook.com>
2018-08-14 15:20:08 +0000

    Add autoencoder sample

---
+++ AI-Workshop/samples/generative_models/README.md
@@ -0,0 +1,32 @@
+# Generative Models
+Collection of generative models, e.g. GAN, VAE.
+
+## Research papers
+### Generative Adversarial Nets (GAN)
+  1. [Vanilla GAN](https://arxiv.org/abs/1406.2661)
+  2. [Conditional GAN](https://arxiv.org/abs/1411.1784)
+  3. [InfoGAN](https://arxiv.org/abs/1606.03657)
+  4. [Wasserstein GAN](https://arxiv.org/abs/1701.07875)
+  5. [Mode Regularized GAN](https://arxiv.org/abs/1612.02136)
+  6. [Coupled GAN](https://arxiv.org/abs/1606.07536)
+  7. [Auxiliary Classifier GAN](https://arxiv.org/abs/1610.09585)
+  8. [Least Squares GAN](https://arxiv.org/abs/1611.04076v2)
+  9. [Boundary Seeking GAN](https://arxiv.org/abs/1702.08431)
+  10. [Energy Based GAN](https://arxiv.org/abs/1609.03126)
+  11. [f-GAN](https://arxiv.org/abs/1606.00709)
+  12. [Generative Adversarial Parallelization](https://arxiv.org/abs/1612.04021)
+  13. [DiscoGAN](https://arxiv.org/abs/1703.05192)
+  14. [Adversarial Feature Learning](https://arxiv.org/abs/1605.09782) & [Adversarially Learned Inference](https://arxiv.org/abs/1606.00704)
+  15. [Boundary Equilibrium GAN](https://arxiv.org/abs/1703.10717)
+  16. [Improved Training for Wasserstein GAN](https://arxiv.org/abs/1704.00028)
+  17. [DualGAN](https://arxiv.org/abs/1704.02510)
+  18. [MAGAN: Margin Adaptation for GAN](https://arxiv.org/abs/1704.03817)
+  19. [Softmax GAN](https://arxiv.org/abs/1704.06191)
+  20. [GibbsNet](https://papers.nips.cc/paper/7094-gibbsnet-iterative-adversarial-inference-for-deep-graphical-models.pdf)
+
+### Variational Autoencoder (VAE)
+  1. [Vanilla VAE](https://arxiv.org/abs/1312.6114)
+  2. [Conditional VAE](https://arxiv.org/abs/1406.5298)
+  3. [Denoising VAE](https://arxiv.org/abs/1511.06406)
+  4. [Adversarial Autoencoder](https://arxiv.org/abs/1511.05644)
+  5. [Adversarial Variational Bayes](https://arxiv.org/abs/1701.04722)
---
+++ AI-Workshop/samples/generative_models/simple_autoencoder.R
@@ -0,0 +1,81 @@
+
+
+#' 
+#' This script demonstrates how to build a autoencoder with Keras.
+#' 
+
+
+library(keras)
+library(dplyr)
+K <- keras::backend()
+
+
+### 0. Parameters ----
+batch_size <- 100L
+
+original_dim <- 784L
+encoding_dim <- 2L # small number of encoding dimension only for visualization purposes
+latent_dim <- 784L
+
+n_epochs <- 10L
+
+
+
+### 1. Model definition ----
+## 1.1. Layers definition
+x <- layer_input(shape = c(original_dim)) # input placeholder
+
+h <- layer_dense(x, encoding_dim , activation = "relu") # encoded representation of the input
+
+decoded_h <- layer_dense(h, latent_dim, activation = "sigmoid") # lossy reconstruction of the input
+
+
+## 1.2. Model maps
+# model maps an input to its reconstruction
+autoencoder <- keras_model(x, decoded_h)
+
+# model maps an input to its encoded representation
+encoder <- keras_model(x, h)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input <- layer_input(shape = c(encoding_dim))
+# retrieve the last layer of the autoencoder model
+decoder_layer <- get_layer(autoencoder, index = -1)
+# create the decoder model
+decoder <- keras_model(encoded_input, decoder_layer(encoded_input))
+
+
+## 1.3. Compile model
+autoencoder %>%
+  compile(optimizer = "adadelta", loss = "binary_crossentropy")
+
+
+
+### 2. Get data and fit model ----
+mnist <- dataset_mnist()
+
+x_train <- mnist$train$x / 255
+x_test <- mnist$test$x / 255
+x_train <- x_train %>% apply(1, as.numeric) %>% t()
+x_test <- x_test %>% apply(1, as.numeric) %>% t()
+
+
+autoencoder %>% fit(x_train, x_train,
+                    epochs = n_epochs,
+                    batch_size = batch_size,
+                    shuffle = T,
+                    validation_data = list(x_test, x_test))
+
+
+
+### 3. Visualizations ----
+library(ggplot2)
+x_test_encoded <- predict(encoder, x_test, batch_size = batch_size)
+
+x_test_encoded %>%
+  as_data_frame %>% 
+  mutate(Class = factor(mnist$test$y)) %>%
+  ggplot(aes(x = V1, y = V2, color = Class)) + 
+  geom_point(alpha = .75)
+
+
