92d508234bdf5feee596e84b8a90464f99b2621b
Ubuntu <dp@spark-u1804-vm.yhcwu2qpzvlevlqi2yj2e2elpd.dx.internal.cloudapp.net>
2020-04-22 15:32:34 +0000

    Add helo spark demo

---
+++ big-data-workshop/hello-spark-demo/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+# start jupyter
+jupyter lab
+#! open jupyter lab terminal in your browser
+
+# check that is not your PC now
+htop
+
+# clone this repo
+git clone https://github.com/codez0mb1e/evangelism.git
+cd evangelism/big-data-workshop/hello-spark-demo
+
+# open this script
+cat start.sh
+
+# check installed frameworks
+python --version
+R --version
+
+pyspark
+exit()
